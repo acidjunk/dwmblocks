d9c340f04939fbeec3d0770f4309c38d60de30d2
s3m <s3m@archlinux.org>
2022-09-21 10:12:47 +0200

    added icons

--- blocks.h
+++ blocks.h
@@ -4,7 +4,7 @@ static const Block blocks[] = {
     {"",            "sb-brightness",        0,                      2},
     {"",            "sb-battery",           5,                      0},
     {"",            "sb-volume",            0,                      1},
-    {"",            "sb-date",              60,                     0},
+    {"",            "sb-clock",              60,                     0},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
